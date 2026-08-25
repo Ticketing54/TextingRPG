1b5fd6bb0b01f2fffadfc2f69a72514fcd6c6750
JinyoungKim <85231269+Ticketing54@users.noreply.github.com>
2026-08-25 11:16:47 +0900

    feat: add KeywordIntroPanel for pre-game keyword selection

---
+++ Assets/02_Script/Runtime/UI/KeywordIntroPanel.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TextingRPG.UI
+{
+    public class KeywordIntroPanel : MonoBehaviour
+    {
+        [SerializeField] string[] availableKeywords =
+            { "안개", "폭풍우", "낡은 지도", "은화", "늑대 울음", "별빛" };
+        [SerializeField] Transform keywordButtonContainer;
+        [SerializeField] Button keywordButtonPrefab;
+        [SerializeField] Button startButton;
+        [SerializeField] int requiredSelectionCount = 3;
+
+        public event Action<string[]> OnKeywordsConfirmed;
+
+        private readonly List<string> _selected = new List<string>();
+
+        private void Awake()
+        {
+            foreach (var keyword in availableKeywords)
+            {
+                var button = Instantiate(keywordButtonPrefab, keywordButtonContainer);
+                button.GetComponentInChildren<TMP_Text>().text = keyword;
+                button.onClick.AddListener(() => ToggleKeyword(keyword, button));
+            }
+
+            startButton.onClick.AddListener(ConfirmSelection);
+            startButton.interactable = false;
+        }
+
+        private void ToggleKeyword(string keyword, Button button)
+        {
+            if (_selected.Contains(keyword))
+            {
+                _selected.Remove(keyword);
+                SetButtonSelectedVisual(button, false);
+            }
+            else if (_selected.Count < requiredSelectionCount)
+            {
+                _selected.Add(keyword);
+                SetButtonSelectedVisual(button, true);
+            }
+
+            startButton.interactable = _selected.Count == requiredSelectionCount;
+        }
+
+        private static void SetButtonSelectedVisual(Button button, bool selected)
+        {
+            var colors = button.colors;
+            colors.normalColor = selected ? new Color(0.75f, 0.85f, 1f) : Color.white;
+            button.colors = colors;
+        }
+
+        private void ConfirmSelection()
+        {
+            OnKeywordsConfirmed?.Invoke(_selected.ToArray());
+            gameObject.SetActive(false);
+        }
+    }
+}
